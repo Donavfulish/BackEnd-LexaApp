bdb08244c4aeae716a4a038eb615e53b3a660434
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-12 16:31:40 +0700

    feat(app): update application

--- src/main/kotlin/api/Application.kt
+++ src/main/kotlin/api/Application.kt
@@ -1,11 +1,13 @@
 package com.lexa
 
+import api.plugins.configureExceptionHandling
 import com.lexa.api.plugins.configureRouting
 import com.lexa.api.plugins.configureSecurity
 import com.lexa.api.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.netty.EngineMain
 import com.lexa.api.config.DatabaseFactory
+import com.lexa.api.plugins.configureMonitoring
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -14,9 +16,10 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     DatabaseFactory.init()
+    configureMonitoring()
     configureSerialization()
-    configureDatabases()
     configureSecurity()
     configureRouting()
+    configureExceptionHandling()
 }
 

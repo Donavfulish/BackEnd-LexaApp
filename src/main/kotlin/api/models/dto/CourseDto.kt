70d2ee2e1ce6040631ba3a06de22a4f7568487a3
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-12 16:30:35 +0700

    feat(model/dto): add mock course dto

--- src/main/kotlin/api/models/dto/CourseDto.kt
+++ src/main/kotlin/api/models/dto/CourseDto.kt
@@ -1,11 +1,25 @@
-package com.lexa.api.models.dto
+package api.models.dto
 
 import kotlinx.serialization.Serializable
 
+// Định nghĩa các data class mà be và fe sẽ sử dụng để giao tiếp với nhau
+// DTO dùng để trả dữ liệu về (Response)
 @Serializable
 data class CourseDto(
-    val id: Int,
+    val id: Long,
+    val topicId: Int?,
     val title: String,
-    val description: String,
-    val topic: String
+    val description: String?,
+    val creatorId: Int,
+    val privacy: String?,
+)
+
+// DTO dùng để nhận dữ liệu từ Client (Request Body cho luồng POST)
+@Serializable
+data class CreateCourseRequest(
+    val topicId: Int? = null,
+    val title: String,
+    val description: String? = null,
+    val creatorId: Int,
+    val privacy: String
 )

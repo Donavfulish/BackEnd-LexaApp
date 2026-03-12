041840b2e2e1da5083c1e27b779129e7d2edd208
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-12 16:31:26 +0700

    feat(route): add mock course route

--- src/main/kotlin/api/routes/CourseRoute.kt
+++ src/main/kotlin/api/routes/CourseRoute.kt
@@ -1,15 +1,38 @@
 package com.lexa.api.routes
 
-import com.lexa.api.repository.CourseRepository
+import api.models.dto.ApiResponse
+import api.models.dto.CreateCourseRequest
+import api.models.dto.successResponse
+import com.lexa.api.services.CoursesService
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.courseRoutes(repository: CourseRepository) {
+fun Route.courseRoutes(coursesService: CoursesService) {
+
     route("/api/courses") {
+
         get {
-            val courses = repository.getAllCourses()
-            call.respond(courses) // Ktor tự động parse List<CourseDto> thành chuỗi JSON
+            val courses = coursesService.getCourses()
+
+            call.respond(
+                HttpStatusCode.OK,
+                successResponse(courses, "Lấy danh sách khóa học thành công")
+            )
+        }
+
+        post {
+
+            val request = call.receive<CreateCourseRequest>()
+
+            val id = coursesService.addCourse(request).getOrThrow()
+
+            call.respond(
+                HttpStatusCode.Created,
+                mapOf("id" to id)
+            )
         }
     }
 }

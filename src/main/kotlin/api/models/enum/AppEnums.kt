f1a0b47353a96ddea37ea5b3c56001303a444e81
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-12 16:29:11 +0700

    feat(models): add system enum

---
+++ src/main/kotlin/api/models/enum/AppEnums.kt
@@ -0,0 +1,9 @@
+package api.models.enum
+
+enum class LanguageType { ENGLISH, VIETNAMESE }
+enum class OtpPurpose { VERIFY_EMAIL, RESET_PASSWORD }
+enum class PrivacyType { PUBLIC, PRIVATE }
+enum class ProgressStatus { REMEMBER, FORGOTTEN }
+enum class ProviderType { GOOGLE, FACEBOOK, GITHUB }
+enum class UserRole { TEACHER, STUDENT }
+enum class VocabType { A1, A2, B1, B2, C1, C2 }

ead754da4c4f1bdf3a59292a0083cd390efdcffc
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-24 22:16:09 +0700

    feat(models): update ParagraphDto

--- src/main/kotlin/api/models/dto/ParagraphDto.kt
+++ src/main/kotlin/api/models/dto/ParagraphDto.kt
@@ -9,3 +9,21 @@ data class ShortParagraphDto(
 ){
 
 }
+
+@Serializable
+data class CreateParagraphRequest(
+    val speakingDayId: Long,
+    val paragraphOrder: Long? = null,
+    val paragraph: String,
+    val audioURL: String? = null
+)
+
+
+
+@Serializable
+data class ParagraphResponseDto(
+    val id: Long,
+    val paragraph: String,
+    val audioUrl: String?,
+    val paragraphOrder: Long? = null,
+)

7eb6cb98fe614acc5780dba8ffdf98924a93e352
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-24 22:24:53 +0700

    feat(repo): add delete and edit paragraph

--- src/main/kotlin/api/repository/ParagraphRepository.kt
+++ src/main/kotlin/api/repository/ParagraphRepository.kt
@@ -2,9 +2,15 @@ package api.repository
 
 import api.models.dto.CreateParagraphRequest
 import api.models.dto.ParagraphResponseDto
+import api.models.dto.UpdateParagraphRequest
+import api.models.tables.SpeakingParagraphResultsTable
 import api.models.tables.SpeakingParagraphsTable
 import com.lexa.api.config.DatabaseFactory.dbQuery
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.update
 
 class ParagraphRepository {
     suspend fun createParagraph(request: CreateParagraphRequest): ParagraphResponseDto = dbQuery {
@@ -21,5 +27,37 @@ class ParagraphRepository {
             paragraphOrder = request.paragraphOrder,
             audioUrl = request.audioURL,
         )
+    }
+
+    suspend fun getParagraphById(paragraphId: Long): ParagraphResponseDto? = dbQuery {
+        SpeakingParagraphsTable.select { SpeakingParagraphsTable.id eq paragraphId }
+            .map {
+                ParagraphResponseDto(
+                    id = it[SpeakingParagraphsTable.id].value,
+                    paragraph = it[SpeakingParagraphsTable.paragraph],
+                    audioUrl = it[SpeakingParagraphsTable.audioUrl],
+                    paragraphOrder = it[SpeakingParagraphsTable.paragraphOrder]
+                )
+            }.singleOrNull()
+    }
+
+    suspend fun updateParagraphInfo(paragraphId: Long, request: UpdateParagraphRequest): Boolean = dbQuery {
+        val updatedRows = SpeakingParagraphsTable.update({ SpeakingParagraphsTable.id eq paragraphId }) {
+            it[speakingDayId] = request.speakingDayId
+            it[paragraphOrder] = request.paragraphOrder
+            // Chỉ update nếu client có truyền giá trị
+            request.paragraph?.let { paragraphValue -> it[paragraph] = paragraphValue }
+            request.audioUrl?.let { audioValue -> it[audioUrl] = audioValue }
+        }
+        updatedRows > 0
+    }
+
+    suspend fun deleteParagraph(paragraphId: Long): Boolean = dbQuery {
+        // Xóa các record liên quan trong bảng Result trước để tránh lỗi Foreign Key Constraint
+        SpeakingParagraphResultsTable.deleteWhere { SpeakingParagraphResultsTable.paragraphId eq paragraphId }
+
+        // Sau đó xóa paragraph chính
+        val deletedRows = SpeakingParagraphsTable.deleteWhere { SpeakingParagraphsTable.id eq paragraphId }
+        deletedRows > 0
     }
 }

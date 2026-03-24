85f0a2dfe6b8261f4fa0724c2857767caa08bd31
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-24 22:24:39 +0700

    feat(models): add delete and edit paragraph type

--- src/main/kotlin/api/models/dto/ParagraphDto.kt
+++ src/main/kotlin/api/models/dto/ParagraphDto.kt
@@ -23,7 +23,22 @@ data class CreateParagraphRequest(
 @Serializable
 data class ParagraphResponseDto(
     val id: Long,
-    val paragraph: String,
+    val paragraph: String?,
     val audioUrl: String?,
+    val paragraphOrder: Long? = null,
+)
+
+@Serializable
+data class UpdateParagraphRequest(
+    val speakingDayId: Long,
+    val paragraphOrder: Long? = null,
+    val paragraph: String? = null,
+    val audioUrl: String? = null
+)
+
+// Request cho API Delete (PATCH /api/paragraph/{paragraphId})
+@Serializable
+data class DeleteParagraphRequest(
+    val speakingDayId: Long,
     val paragraphOrder: Long? = null,
 )
